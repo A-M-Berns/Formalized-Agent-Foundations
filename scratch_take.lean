f81985b9ae162ca4ac5fd56e5bbe4ea507ed35b7
A. M. Berns <ansonberns@gmail.com>
2026-07-27 00:51:47 -0400

    Remove stray scratch file from subagent merge

--- scratch_take.lean
+++
@@ -1,8 +0,0 @@
-import Mathlib.Data.List.TakeDrop
-#check @List.take_append
-#check @List.take_of_length_le
-#check @List.take_succ_cons
-#check @List.take_length
-example (l₁ l₂ : List ℕ) (j : ℕ) :
-    (l₁ ++ l₂).take j = l₁.take j ++ l₂.take (j - l₁.length) := by
-  exact? says exact List.take_append l₁ l₂ j
