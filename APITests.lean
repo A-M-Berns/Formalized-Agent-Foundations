199aca9ad9a4de6a47fd3c248c8524d985553598
A. M. Berns <ansonberns@gmail.com>
2026-08-17 23:28:55 -0400

    harness: APITests for FactoredSpaces

--- APITests.lean
+++ APITests.lean
@@ -2,5 +2,6 @@ import APITests.LogicalInduction
 import APITests.ModalAgents
 import APITests.CartesianFrames
 import APITests.FiniteFactoredSets
+import APITests.FactoredSpaces
 
 /-! Client-style compilation tests for every completed paper's supported API. -/
